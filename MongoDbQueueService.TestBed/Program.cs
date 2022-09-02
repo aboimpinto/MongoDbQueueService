55ba8c8b1059fd93c42752839fabb0d6b18dc391
Paulo Aboim Pinto <aboimpinto@gmail.com>
2022-09-02 16:24:53 +0200

    Publisher constructor overload

--- MongoDbQueueService.TestBed/Program.cs
+++ MongoDbQueueService.TestBed/Program.cs
@@ -1,7 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MongoDbQueueService.Configuration;
 
+/*
+var subscriber = new MongoDbQueueService.Subscriber(
+    "mongodb://localhost:27017",
+    "TestBed",
+    "TestQueue",
+    "WORKER_1");
+*/
+
+
+/*
+var settings = new SubscriberSettings
+{
+    ConnectionString = "mongodb://localhost:27017",
+    Database = "TestBed",
+    Queue = "TestQueue",
+    WorkerName = "WORKER_1"
+};
+var subscriber = new MongoDbQueueService.Subscriber(settings);
+*/
 
 var subscriber = new MongoDbQueueService.Subscriber();
 
+/*
+var publisher = new MongoDbQueueService.Publisher(
+    "mongodb://localhost:21017",
+    "TestBed",
+    "TestQueue"
+);
+*/
 
+/*
+var publisherSettings = new PublisherSettings
+{
+    ConnectionString = "mongodb://localhost:27107",
+    Database = "TestBed",
+    Queue = "TestQueue"
+};
+var publisher = new MongoDbQueueService.Publisher(publisherSettings);
+*/
+
+var publisher = new MongoDbQueueService.Publisher();
--- MongoDbQueueService/Publisher.cs
+++ MongoDbQueueService/Publisher.cs
@@ -1,9 +1,12 @@
 using System;
+using System.IO;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using MongoDbQueueService.Configuration;
 
 namespace MongoDbQueueService
 {
@@ -11,17 +14,38 @@ namespace MongoDbQueueService
     {
         private IMongoDatabase _database;
         private IMongoCollection<QueueCollection> _queueCollection;
-        private readonly Semaphore _openConnectionSemaphore;
+        private Semaphore _openConnectionSemaphore;
+
+        public Publisher()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+                .Build();
+
+            var publishSettings = new PublisherSettings();
+            configuration.Bind("PublisherSettings", publishSettings);
+
+            this.ConnectDatabase(
+                publishSettings.ConnectionString,
+                publishSettings.Database,
+                publishSettings.Queue);
+        }
+
+        public Publisher(PublisherSettings settings)
+        {
+            this.ConnectDatabase(
+                settings.ConnectionString,
+                settings.Database,
+                settings.Queue);
+        }
 
         public Publisher(string url, string database, string collection)
         {
-            var client = new MongoClient(url);
-            this._database = client.GetDatabase(database);
-            this._queueCollection = this._database.GetCollection<QueueCollection>(collection);
-
-            this._openConnectionSemaphore = new Semaphore(
-                client.Settings.MaxConnectionPoolSize / 2, 
-                client.Settings.MaxConnectionPoolSize / 2);
+            this.ConnectDatabase(
+                url, 
+                database, 
+                collection);
         }
 
         public Task SendAsync<T>(T payload, int priority)
@@ -41,6 +65,17 @@ namespace MongoDbQueueService
             };
 
             await this.ThrottlingPipeline<Task>(() => this._queueCollection.InsertOneAsync(item));
+        }
+
+        private void ConnectDatabase(string url, string database, string collection)
+        {
+            var client = new MongoClient(url);
+            this._database = client.GetDatabase(database);
+            this._queueCollection = this._database.GetCollection<QueueCollection>(collection);
+
+            this._openConnectionSemaphore = new Semaphore(
+                client.Settings.MaxConnectionPoolSize / 2, 
+                client.Settings.MaxConnectionPoolSize / 2);
         }
 
         private async Task ThrottlingPipeline<T>(Func<Task> task)
